6d090892581158f9b1c914f5250d853135a950b9
Claudius King <tezcatlipoca7123@gmail.com>
2019-04-30 10:30:36 -0700

    Added bootstrap styling

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ function onload(){
   getNewsData();
   getPoem("happy");
   $(".happy").on("click",switchToAmbience);
+  $(".sad").on("click",switchToAmbience);
+  $(".chill").on("click",switchToAmbience);
+  $(".hype").on("click",switchToAmbience);
+  $(".romantic").on("click",switchToAmbience);
+  $(".confident").on("click",switchToAmbience);
 }
 function getNewsData(){
   ajaxOptions = {
