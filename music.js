62cd817d0ba330bb6fd09ebf343ab9a0e3cf0f61
Marlon Clay <luciousmc@icloud.com>
2019-04-30 11:26:04 -0700

    adds youtube videos by search term

---
+++ music.js
@@ -0,0 +1,34 @@
+
+
+class bgMusic{
+    constructor( ) {
+        this.handleAjaxError = this.handleAjaxError.bind(this);
+        this.handleSuccess = this.handleSuccess.bind(this);
+    }
+    getMusic( mood ) {
+        let ajaxOptions = {
+            url: 'http://s-apis.learningfuze.com/hackathon/youtube/search.php',
+            method: 'post',
+            dataType: 'json',
+            data: {
+                q: `${mood} songs`,  // the string to search for
+                maxResults: 10, // how many results to get
+                // detailLevel: 'verbose', // default is low. how much information you will get about the video
+            },
+            success: this.handleSuccess,
+            error: this.handleAjaxError
+        };
+        $.ajax(ajaxOptions);
+    }
+    handleSuccess( result ){
+        debugger;
+        if (result.success) {
+            console.log('the result is...', result);
+        } else {
+            console.log('connection made, but request was denied...', result);
+        }
+    }
+    handleAjaxError( result ){
+        console.log('the error object...', result);
+    }
+}
