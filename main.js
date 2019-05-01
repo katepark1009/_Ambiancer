323455b1fffd9f430fba9be95e994cd2d6a55b9e
Kate Park <rudhi1009@gmail.com>
2019-04-30 18:12:32 -0700

    images js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function onload(){
   $(".hype").on("click",function(){switchToAmbience("hype");});
   $(".romantic").on("click",function(){switchToAmbience("romantic");});
   $(".confident").on("click",function(){switchToAmbience("confident");});
+  $(".goback").on("click", function(){returnToMain();});
 }
 function switchToAmbience(mood){
   $(".home-screen").hide();
@@ -20,4 +21,8 @@ function switchToAmbience(mood){
   text.getNewsData(mood);
   text.getPoems(mood);
   music.getMusic(mood);
+}
+function returnToMain(){
+  $(".home-screen").show();
+  $('#fullpage').hide();
 }
