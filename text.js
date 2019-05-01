a5e10479ceeeb9d773defd3fb258551fbe119eb5
Claudius King <tezcatlipoca7123@gmail.com>
2019-05-01 12:01:25 -0700

    News feed of titles comes up and takes the user to the appropriate article

--- text.js
+++ text.js
@@ -1,6 +1,8 @@
 class Text{
   constructor(){
     this.articles = [];
+    this.newsDataSuccess = this.newsDataSuccess.bind(this);
+    this.handleTitleClick = this.handleTitleClick.bind(this);
   }
   getNewsData(query, day=30, month=4, year=2019){
     let formattedDate = year+"-"+month+"-"+day;
@@ -54,29 +56,22 @@ class Text{
   newsDataSuccess(response){
     $(".article-title").empty();
     $(".article-author").empty();
-    console.log("articles:",response);
-    this.articles = response.articles;
     let randomIndex;
     while(response.articles.length > 0){
       randomIndex = Math.floor(Math.random()*response.articles.length);
-      let article = response.articles.splice(randomIndex,1);
-      this.articles.push(article);
-      let articleTitle = $("<h1>").text(response.articles[randomIndex].title);
-      articleTitle.on("click",handleTitleClick);
-    }
+      let article = response.articles.splice(randomIndex,1)[0];
+      if(article.content){
+        this.articles.push(article);
+        let articleTitle = $("<p>").text(article.title);
+        let firstBr = $("<br>");
+        let secondBr = $("<br>");
+        articleTitle.on("click",this.handleTitleClick);
+        $(".news-feed").append(articleTitle).append(firstBr).append(secondBr);
+      }
+      else{
 
-    let articleAuthor;
-    if(response.articles[randomIndex].author){
-      articleAuthor = $("<h2>").text("Author: "+response.articles[randomIndex].author);
+      }
     }
-    else{
-      articleAuthor = $("<h2>").text("Author: Unknown");
-    }
-    $(".article-title").append(articleTitle);
-    $(".article-author").append(articleAuthor);
-    let textIndex = response.articles[randomIndex].content.indexOf("[+");
-    let formattedText = response.articles[randomIndex].content.substring(0,textIndex);
-    $(".article-text").html("<p class='text'>"+formattedText+"</p>");
   }
   poemSuccess(response){
     $(".poem-title").empty();
@@ -127,29 +122,31 @@ class Text{
     preString += postString;
     $(".poem-text").html("<p class='text'>"+preString+"</p>");
   }
-}
-function handleTitleClick(){
-  $(".section2").empty();
-  let title = $(this).text();
-  let chosenArticle;
-  for(let article of this.articles){
-    if(article.title === title){
-      chosenArticle = article;
-      break;
-    }
-  }
-  let row1 = $("<div>").addClass("row1 row");
-  let row2 = $("<div>").addClass("row2 row");
-  let row3 = $("<div>").addClass("row3 row");
-  let row4 = $("<div>").addClass("row4 row");
-  let articleAuthor = $("<div>").addClass("col-12 article-author").text(chosenArticle.author);
-  let articleInfo = $("<div>").addClass("col-12 mini-div article-info");
-  let articleText = $("<div>").addClass("col-12 mini-div article-text").text(chosenArticle.content);
-  let articleTitle = $("<div>").addClass("col-12 article-title").text(chosenArticle.title);
-  $(".section2").append(row1).append(row2);
-  $(".row1").append(articleInfo);
-  $(".article-info").append(row3).append(row4);
-  $(".row3").append(articleTitle);
-  $(".row4").append(articleAuthor);
-  $(".row2").append(articleText);
+  handleTitleClick(event){
+   $(".section2").empty();
+   let title = $(event.target).text();
+   let chosenArticle;
+   for(let article of this.articles){
+     if(article.title === title){
+       chosenArticle = article;
+       break;
+     }
+   }
+   let textIndex = chosenArticle.content.indexOf("[+");
+   let formattedText = chosenArticle.content.substring(0,textIndex);
+   let row1 = $("<div>").addClass("row1 row");
+   let row2 = $("<div>").addClass("row2 row");
+   let row3 = $("<div>").addClass("row3 row");
+   let row4 = $("<div>").addClass("row4 row");
+   let articleAuthor = $("<div>").addClass("col-12 article-author").text(chosenArticle.author);
+   let articleInfo = $("<div>").addClass("col-12 mini-div article-info");
+   let articleText = $("<div>").addClass("col-12 mini-div article-text").text(formattedText);
+   let articleTitle = $("<div>").addClass("col-12 article-title").text(chosenArticle.title);
+   $(".section2").append(row1).append(row2);
+   $(".row1").append(articleInfo);
+   $(".article-info").append(row3).append(row4);
+   $(".row3").append(articleTitle);
+   $(".row4").append(articleAuthor);
+   $(".row2").append(articleText);
+ }
 }
