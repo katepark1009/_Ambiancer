585f3a0363b956485eb0362a9b5a0ba22a63ebec
Claudius King <tezcatlipoca7123@gmail.com>
2019-05-04 16:45:28 -0700

    news articles are not random in presentation mode

--- text.js
+++ text.js
@@ -6,6 +6,7 @@ class TextDataHandler{
     this.handleTitleClick = this.handleTitleClick.bind(this);
     this.handleNewsfeedButton = this.handleNewsfeedButton.bind(this);
     this.mood = "";
+    this.newsResponse = [];
   }
   getNewsData(query, day=30, month=4, year=2019){
     this.mood = query;
@@ -13,12 +14,12 @@ class TextDataHandler{
     let search = "";
     if(presentationMode){
       let formattedQuery = {
-        happy:"thanks",
+        happy:"heartwarming",
         sad:"tragic",
-        chill:"vacation",
-        hype:"party",
-        romantic:"love",
-        motivated:"success"
+        chill:"clouds",
+        hype:"amazing",
+        romantic:"infatuation",
+        motivated:"workout"
       }
       search = formattedQuery[query];
     }
@@ -28,7 +29,7 @@ class TextDataHandler{
         sad:["dead","accident","tragic"],
         chill:["cafe","vacation"],
         hype:["party","intense"],
-        romantic:["love","flower"],
+        romantic:["love",""],
         motivated:["business","success"]
       }
       let randomIndex = getRandomIndex(formattedQuery[query]);
@@ -57,7 +58,7 @@ class TextDataHandler{
       let formattedQuery = {
         happy:"joy",
         sad:"funeral",
-        chill:"relax",
+        chill:"coffee",
         hype:"party",
         romantic:"love",
         motivated:"business"
@@ -87,62 +88,29 @@ class TextDataHandler{
     $.ajax(ajaxOptions);
   }
   newsDataSuccess(response){
+    this.newsResponse = response;
     if(presentationMode){
       console.log('response articles:', response.articles);
       $(".article-title").empty();
       $(".article-author").empty();
-      let article1;
-      let article2;
-      let article3;
-      let article4;
-      let article5;
-      let articleTitle1;
-      let articleTitle2;
-      let articleTitle3;
-      let articleTitle4;
-      let articleTitle5;
       switch(this.mood){
         case "happy":
-          article1 = response.articles[7];
-          this.articles.push(article1);
-          articleTitle1 = $("<p>").addClass("text newsfeed-title neon-text").text(article1.title);
-          articleTitle1.on("click",this.handleTitleClick);
-          $(".news-feed").append(articleTitle1);
+            this.presentationTitles(9,16,15,17,19);
           break;
         case "sad":
-          article1 = response.articles[7];
-          this.articles.push(article1);
-          articleTitle1 = $("<p>").addClass("text newsfeed-title neon-text").text(article1.title);
-          articleTitle1.on("click",this.handleTitleClick);
-          $(".news-feed").append(articleTitle1);
-          article2 = response.articles[7];
-          this.articles.push(article2);
-          articleTitle2 = $("<p>").addClass("text newsfeed-title neon-text").text(article2.title);
-          articleTitle2.on("click",this.handleTitleClick);
-          $(".news-feed").append(articleTitle1);
-          article3 = response.articles[7];
-          this.articles.push(article3);
-          articleTitle3 = $("<p>").addClass("text newsfeed-title neon-text").text(article3.title);
-          articleTitle3.on("click",this.handleTitleClick);
-          $(".news-feed").append(articleTitle1);
-          article4 = response.articles[7];
-          this.articles.push(article1);
-          articleTitle4 = $("<p>").addClass("text newsfeed-title neon-text").text(article4.title);
-          articleTitle4.on("click",this.handleTitleClick);
-          $(".news-feed").append(articleTitle1);
-          article5 = response.articles[7];
-          this.articles.push(article1);
-          articleTitle5 = $("<p>").addClass("text newsfeed-title neon-text").text(article5.title);
-          articleTitle5.on("click",this.handleTitleClick);
-          $(".news-feed").append(articleTitle1);
+            this.presentationTitles(7,10,11,15,17);
           break;
         case "chill":
+            this.presentationTitles(2,5,7,13,17);
           break;
         case "hype":
+            this.presentationTitles(1,2,3,10,19);
           break;
         case "romantic":
+            this.presentationTitles(7,8,13,16,19);
           break;
         case "motivated":
+            this.presentationTitles(13,14,15,16,17);
       }
 
 
@@ -174,6 +142,7 @@ class TextDataHandler{
     console.log("poems:",response);
     this.poems = response;
     let randomIndex = getRandomIndex(response);
+    console.log("the chosen poem's index is:",randomIndex);
     let poemTitle = $("<h1>").addClass("neon-text").text(response[randomIndex].title);
     let poemAuthor = $("<h2>").addClass("neon-text").text("- Author: "+response[randomIndex].author+ " -" )
     $(".poem-title").append(poemTitle);
@@ -262,4 +231,19 @@ class TextDataHandler{
      $(".news-feed").append(articleTitle);
    }
  }
+  presentationTitle(index){
+    let article1 = this.newsResponse.articles[index];
+    this.articles.push(article1);
+    let articleTitle1 = $("<p>").addClass("text newsfeed-title neon-text").text(article1.title);
+    articleTitle1.on("click",this.handleTitleClick);
+    $(".news-feed").append(articleTitle1);
+  }
+  presentationTitles(index1,index2,index3,index4,index5){
+    this.presentationTitle(index1);
+    this.presentationTitle(index2);
+    this.presentationTitle(index3);
+    this.presentationTitle(index4);
+    this.presentationTitle(index5);
+  }
+
 }
