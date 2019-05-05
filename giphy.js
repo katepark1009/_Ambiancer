b4e00c687593f69291d1766c6a909f860b629b05
Claudius King <tezcatlipoca7123@gmail.com>
2019-05-05 16:38:01 -0700

    Got the gifs and text ready for presentation mode

--- giphy.js
+++ giphy.js
@@ -15,7 +15,7 @@ class Giphy{
             'sad': ['sad', 'gloomy'],
             'motivated': ['successful', 'achievement'],
             'chill': ['chill', 'free'],
-            'romantic': ['lovely', 'rose', 'wedding'],
+            'romantic': ['infatuated'],//, 'rose', 'wedding'],
             'hype': ['urban', 'hype', 'city']
         };
         var randomIndex = getRandomIndex(this.ambience[this.mood]);
@@ -28,13 +28,37 @@ class Giphy{
             },
             success: this.getDataSuccess,
             error: handleError
-         };
+        };
         ajaxOptions.data['api_key'] = keys.giphy;
         ajaxOptions.data['tag'] = this.ambience[this.mood][randomIndex];
         $.ajax( ajaxOptions );
     }
     getDataSuccess(data){ //display gif on DOM
-        this.newGif = data.data.image_url;
+        if(presentationMode){
+          switch(this.mood){
+            case "happy":
+              this.newGif = "https://media2.giphy.com/media/l41YbnQ2qRrBesVag/giphy.gif";
+              break;
+            case "sad":
+              this.newGif = "https://media2.giphy.com/media/efYqX6r8prnd6/giphy.gif";
+              break;
+            case "chill":
+              this.newGif = "https://media2.giphy.com/media/HpuTckGF0corm/giphy.gif";
+              break;
+            case "hype":
+              this.newGif = "https://media3.giphy.com/media/cOIauOKrdWkiigzFSS/giphy.gif";
+              break;
+            case "romantic":
+              this.newGif = "https://media3.giphy.com/media/46zBmNyfiLAV9OszUs/giphy.gif";
+              break;
+            case "motivated":
+              this.newGif = "https://media0.giphy.com/media/MuHYrH0tOGF0DTjEMU/giphy.gif";
+              break;
+          }
+        }
+        else{
+          this.newGif = data.data.image_url;
+        }
         this.newDiv = $('<div>').addClass('gifs').css("background-image", "url(" + this.newGif + ")");
         $('.gif-container').append(this.newDiv);
     }
